ee0838a0862adccb3c8ffa24bfc523c5ac2b9e99
Rokas Rudzianskas <38469892+RokasRudzenskas@users.noreply.github.com>
2021-03-10 22:01:11 +0200

    Finally commit

---
+++ main.js
@@ -0,0 +1,69 @@
+
+let entries = [];
+const goal = 25;
+const  entriesWrapper = document.querySelector("#entries");
+document.querySelector('#target').innerText = goal;
+
+function addNewEntry(newEntry) {
+  entriesWrapper.removeChild(entriesWrapper.firstElementChild);
+  // console.log(newEntry);
+const listItem = document.createElement('li');
+  const listValue = document.createTextNode(newEntry.toFixed(1));
+  listItem.appendChild(listValue);
+
+  entriesWrapper.appendChild(listItem);
+
+}
+
+function reducer(total, currentValue) {
+  return total + currentValue;
+}
+
+function calcTotal() {
+  const totalValue = entries.reduce(reducer).toFixed(1);
+  document.getElementById('total').innerText = totalValue;
+  document.getElementById('progressTotal').innerText = totalValue;
+}
+
+function calcAverage() {
+  const average = (entries.reduce(reducer) / entries.length).toFixed(1);
+  document.getElementById('average').innerText = average;
+}
+
+function weeklyHigh() {
+  const high = Math.max(...entries);
+document.getElementById("high").innerText = high;
+}
+
+function calcGoal() {
+  const totalValue = entries.reduce(reducer).toFixed(1);
+  const completedPercent = totalValue / (goal / 100);
+  const progressCircle = document.querySelector("#progressCircle");
+  if(completedPercent > 100) completedPercent === 100;
+  progressCircle.style.background = `conic-gradient(#70db70 ${completedPercent}, #2d3740 ${completedPercent}% 100%)`;
+}
+function handleSubmit(event){
+  event.preventDefault();
+  const entry = Number(document.querySelector('#entry').value);
+  // console.log(entry);
+if(!entry){
+  return;
+
+
+}
+
+
+document.querySelector("form").reset();
+entries.push(entry);
+  // console.log(entries)
+  addNewEntry(entry);
+  calcTotal();
+  calcAverage();
+  weeklyHigh();
+  calcGoal();
+
+
+}
+
+const form = document.querySelector("form").addEventListener("submit", handleSubmit);
+// alert("Hello")
